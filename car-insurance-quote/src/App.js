341916f1e034604b6d240cdd736b206a2149bc6d
RancellTapia <owner@MacBook.local>
2020-03-06 18:59:52 -0500

    Starting the project

--- car-insurance-quote/src/App.js
+++ car-insurance-quote/src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div `
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div `
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 
 function App() {
   return (
-    <h1>Hola</h1>
-    <p>Soy humano</p>
+    <Contenedor>
+      <Header
+        titulo = 'Car Insurance Quote'
+      />
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenedor>
+   
+
   );
 }
 
---
+++ car-insurance-quote/src/components/Formulario.js
@@ -0,0 +1,126 @@
+import React, { useState }from 'react';
+import styled from '@emotion/styled';
+
+const Campo = styled.div `
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+const Label = styled.label `
+    flex: 0 0 100px;
+`;
+const Select = styled.select `
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+const InputRadio = styled.input `
+    margin: 0 1rem;
+`;
+
+const Boton = styled.button `
+    background-color: #00838F;
+    font-size: 18px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .8s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
+const Formulario = () => {
+
+    const [ datos, guardarDatos ] = useState ({
+        marca: '',
+        year: '',
+        plan: ''
+    });
+
+    // Extraer los valores del state
+    const { marca, year, plan } = datos;
+
+    // Leer los datos del formulario y colocarlos en el state
+
+    const obtenerInformacion = e => {
+        guardarDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    return (
+        <form>
+            <Campo>
+                <Label>Marca </Label>
+                <Select
+                    name = "marca"
+                    value = {marca}
+                    onChange = {obtenerInformacion}
+                >
+                    <option value = "">-- Seleccione --</option>
+                    <option value = "americano">Americano</option>
+                    <option value = "europeo">Europeo</option>
+                    <option value = "asiatico">Asiatico</option>
+                </Select>
+            </Campo>
+
+            <Campo>
+                <Label>Ano </Label>
+                <Select
+                name = "year"
+                value = {year}
+                onChange = {obtenerInformacion}
+                >
+                    <option value = "">-- Seleccione --</option>
+                    <option value = "2021">2021</option>
+                    <option value = "2020">2020</option>
+                    <option value = "2019">2019</option>
+                    <option value = "2018">2018</option>
+                    <option value = "2017">2017</option>
+                    <option value = "2016">2016</option>
+                    <option value = "2015">2015</option>
+                    <option value = "2014">2014</option>
+                    <option value = "2013">2013</option>
+                    <option value = "2012">2012</option>
+                    <option value = "2011">2011</option>
+                    <option value = "2010">2010</option>
+                    <option value = "2009">2009</option>
+                </Select>
+            </Campo>
+
+            <Campo>
+                <Label>Plan </Label>
+                <InputRadio
+                    type = "radio"
+                    name = "plan"
+                    value = "Basico"
+                    checked = {plan === "basico"}
+                    onChange = {obtenerInformacion}
+                /> Basico 
+
+                <InputRadio
+                    type = "radio"
+                    name = "plan"
+                    value = "completo"
+                    checked = {plan === "completo"}
+                    onChange = {obtenerInformacion}
+                /> Completo 
+
+            </Campo>
+
+            <Boton type = "button">Cotizar</Boton>
+        </form>
+      );
+}
+ 
+export default Formulario;
