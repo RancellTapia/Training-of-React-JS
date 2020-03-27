51b5bf1b3d574ed0b3d4fc18f1bbc843dee9e473
RancellTapia <owner@MacBook.local>
2020-03-27 17:26:51 -0400

    Finishing the login and new account components

--- MERN/cliente/src/components/auth/Login.js
+++ MERN/cliente/src/components/auth/Login.js
@@ -1,8 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
+
+    //State para iniciar Sesion
+    const [ usuario, guardarUsuario ] = useState({
+        email: '',
+        password: ''
+    });
+
+    //Extraer de usuario
+    const { email, password } = usuario;
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+
+    }
+
+    //Cuando el usuario quiere iniciar sesion
+    const onSubmit = e => {
+        e.preventDefault();
+
+        //Validar que no haya campos vacios
+
+        //Pasarlo al action
+    }
+
     return (  
-        <h1>Desde Login</h1>
+        <div className = "form-usuario">
+            <div className = "contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form
+                    onSubmit = {onSubmit}
+                >
+                    <div className = "campo-form">
+                        <label htmlFor = "email">Email</label>
+                        <input
+                            type = "email"
+                            id = "email"
+                            name = "email"
+                            placeholder = "Tu Email"
+                            value = {email}
+                            onChange = {onChange}
+                        />
+                    </div>
+
+                    <div className = "campo-form">
+                        <label htmlFor = "password">Password</label>
+                        <input
+                            type = "password"
+                            id = "password"
+                            name = "password"
+                            placeholder = "Tu Password"
+                            value = {password}
+                            onChange = {onChange}
+                        />
+                    </div>
+
+                    <div className = "campo-form">
+                        <input 
+                            type = "submit"
+                            className = "btn btn-primario btn-block"
+                            value = "Iniciar Sesión"
+                        />
+                    </div>
+                </form>
+
+                <Link to ={'/nueva-cuenta'} className = "enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
  
