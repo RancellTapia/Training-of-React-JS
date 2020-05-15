81ebf23414eb25d1225c3b33385a6422423a0f3a
RancellTapia <owner@MacBook.local>
2020-05-15 18:42:19 -0400

    Adding products to API

---
+++ crud-redux/src/actions/productoActions.js
@@ -0,0 +1,43 @@
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+import clienteAxios from '../config/axios';
+
+// Crear nuevos productos
+export function crearNuevoProductoAction(producto) {
+    return async (dispatch) => {
+        dispatch( agregarProducto() );
+
+        try {
+            // Insertar en la API
+            await clienteAxios.post('./productos', producto);
+
+            // Si todo sale bien, actualiza el state
+            dispatch ( agregarProductoExito(producto) );
+        } catch (error) {
+            console.log(error);
+
+            // Si hay un error cambiar el state
+            dispatch ( agregarProductoError(true) );
+        }
+    }
+}
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+})
+
+// Si el producto se guarda en la base de datos
+const agregarProductoExito = producto => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+// Si hubo un error
+const agregarProductoError = estado => ({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
